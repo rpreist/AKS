597ec4197ee53cce18019f80521cd152323baf09
mr_robot <rpreist58@gmail.com>
2024-02-22 19:40:38 -0600

    container

--- K8s/hello/install.ps1
+++ K8s/hello/install.ps1
@@ -1,6 +1,13 @@
+# Retrieve the storage account keys and convert from JSON
+$keys = az storage account keys list `
+  --resource-group "com.icondigiital" `
+  --account-name "mykontainer" | ConvertFrom-Json
+
+# Run Helm upgrade with the retrieved storage account key
 helm upgrade `
-    --install `
-    --namespace icondigiital `
-    --create-namespace `
-    icondigiital `
-    "./"
+  --install `
+  --namespace icondigiital `
+  --create-namespace `
+  --set "storageAccountKey=$($keys[0].value)" `
+  icondigiital `
+  ./
